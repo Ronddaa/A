dd2663a0ef452ac80446fba60d2abf4bb2369473
Ronddaa <tronddaa@gmail.com>
2024-02-12 20:50:11 +0100

    asd

--- willaDesc.js
+++ willaDesc.js
@@ -3,16 +3,16 @@ let offsetWillaDesc = 0;
 const wrapperW = document.querySelector('.wrapperWDesc');
 
 document.querySelector('#btnSlideWillaNext').addEventListener('click', function () {
-    offsetWillaDesc -= 560;
-    if (offsetWillaDesc < -5500) {
+    offsetWillaDesc -= 660;
+    if (offsetWillaDesc < -6600) {
         offsetWillaDesc = 0
     }
     wrapperW.style.left = offsetWillaDesc + 'px'
 });
 document.querySelector('#btnSlideWillaPrev').addEventListener('click', function () {
-    offsetWillaDesc += 560;
+    offsetWillaDesc += 660;
     if (offsetWillaDesc > 0) {
-        offsetWillaDesc = -5500
+        offsetWillaDesc = -6600
     }
     wrapperW.style.left = offsetWillaDesc + 'px'
 });
