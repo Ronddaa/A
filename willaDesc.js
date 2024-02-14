88846e4183f0c8107873bd6fed2923f0a21af23b
Ronddaa <tronddaa@gmail.com>
2024-02-14 10:40:52 +0100

    asd

--- willaDesc.js
+++ willaDesc.js
@@ -2,14 +2,14 @@ let offsetWillaDesc = 0;
 
 const wrapperW = document.querySelector('.wrapperWDesc');
 
-document.querySelector('#btnSlideWillaNext').addEventListener('click', function () {
+document.querySelector('#btnSlideWillaNextDesc').addEventListener('click', function () {
     offsetWillaDesc -= 660;
     if (offsetWillaDesc < -6600) {
         offsetWillaDesc = 0
     }
     wrapperW.style.left = offsetWillaDesc + 'px'
 });
-document.querySelector('#btnSlideWillaPrev').addEventListener('click', function () {
+document.querySelector('#btnSlideWillaPrevDesc').addEventListener('click', function () {
     offsetWillaDesc += 660;
     if (offsetWillaDesc > 0) {
         offsetWillaDesc = -6600
